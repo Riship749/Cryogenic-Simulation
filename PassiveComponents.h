3b64e62fd85b09a4117b5079afae7d7ef9685b07
Riship749 <Riship749@gmail.com>
2026-03-31 18:47:16 -0400

    added stuff for resistors

--- PassiveComponents.h
+++ PassiveComponents.h
@@ -16,7 +16,7 @@ class Capacitor: public Component {
 public:
     double capacitance; 
     double pi = std::acos(-1.0);
-    using Component::Component; // sublass
+    using Component::Component; // sublass 
     Complex getImpedance(double freq, double temp) {
 
     }
